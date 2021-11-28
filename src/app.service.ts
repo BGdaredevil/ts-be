af8dd92f80f682e2f0b2027e62ced8752eaf6013
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-11-28 19:33:53 +0200

    cleanup

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return 'Hello from the most annoying API ever -- cannot generate JS (even though compiles down to js)';
+    return 'Hello from the API that compiles down to js';
   }
 }
